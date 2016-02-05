f85f073ed586d6fc97f52b02b0fa50facabd8cf3
Watch1k <host.sf@gmail.com>
2016-02-05 14:48:08 +0200

    header rdy

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,17 @@
-// head.ready(function(){
+head.ready(function(){
+
+	// detect scroll (for header)
+	function fixedHeader() {
+		if(!$(window).scrollTop() == 0) {
+			$('.header').addClass('is-fixed');
+		} else {
+			$('.header').removeClass('is-fixed');
+		}
+	}
+	fixedHeader();
+	$(window).scroll(function(){
+    	fixedHeader();
+  	});
 
 // // Clear placeholder
 // 	(function() {
@@ -56,4 +69,4 @@
 // 	}, false);
 	
 	
-// });
+});
